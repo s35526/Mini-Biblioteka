6d49cf825ce5e24b25e20072967dd723e2e42c91
s35526 <s35526@pjwstk.edu.pl>
2026-03-18 09:56:16 +0100

    kolejna poprawa i dodanie książek tam gdzie powinno

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
+ksiazka k1 = new ksiazka ("Potop"," Henryk Sienkiewicz", 900, true);
+ksiazka k2 = new ksiazka ("Lalka", "Bolesław Prus", 700, false);
+ksiazka k3 - new ksiazka ("1984", "George Orwell", 300, true);
+
+k1.wypiszInfo();
+k1.wypozycz();
+k1.wypisz.Info();
+k1.zwroc();
+
+
+
