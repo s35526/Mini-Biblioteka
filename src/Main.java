fbaa3e549d5605be8cd2b8b6d877b33ee62f9aef
s35526 <s35526@pjwstk.edu.pl>
2026-03-18 09:00:54 +0100

    Aktualizacja pliku Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Ksiazka {
+    private String tytul;
+    private String autor;
+    private int liczbaStron;
+    private boolean dostepna;
+    public Ksiazka(String tytul, String autor, int liczbaStron, boolean dostepna) {
+        // implementacja konstruktora
+    }
+}
+
