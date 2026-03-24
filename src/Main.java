44dae136bdb1c191b5b6627fa06002c8562c4a07
s35526 <s35526@pjwstk.edu.pl>
2026-03-24 22:05:45 +0100

    wykonałem wszystkie polecenia w zadaniu 4

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 ksiazka k1 = new ksiazka ("Potop"," Henryk Sienkiewicz", 900, true);
 ksiazka k2 = new ksiazka ("Lalka", "Bolesław Prus", 700, false);
-ksiazka k3 - new ksiazka ("1984", "George Orwell", 300, true);
+ksiazka k3 = new ksiazka ("1984", "George Orwell", 300, true);
 
 k1.wypiszInfo();
 k1.wypozycz();
@@ -12,11 +12,43 @@ k1.zwroc();
 
     }
 Czytelnik c1 = new Czytelnik("Jan", "Nowak", 1000, 0);
-Czytelnik c2= new Czytelnik("Asia", "Bąk", 2000, 2);
+Czytelnik c2 = new Czytelnik("Asia", "Bąk", 2000, 2);
 
 c1.wypiszDane();
 c1.zwiekszLiczbeWypozyczen();
 
 c2.wypiszDane();
 c2.zmiejszLiczbeWypozyczen();
-}
+
+Biblioteka biblioteka = new Biblioteka(10);
+        biblioteka.dodajKsiazke(k1);
+        biblioteka.dodajKsiazke(k2);
+        biblioteka.dodajKsiazke(k3);
+
+        System.out.println("Dostępne książki przed wypożyczeniem:");
+        biblioteka.wypiszDostepneKsiazki();
+        System.out.println("Liczba dostępnych: " + biblioteka.policzDostepneKsiazki());
+
+        // Scenariusz wypożyczenia
+        System.out.println("\n=== Wypożyczanie ===");
+        biblioteka.wypozyczKsiazke("Potop", c1);   
+        biblioteka.wypozyczKsiazke("Lalka", c2);   
+
+        System.out.println("\n=== Dostępne książki po wypożyczeniu ===");
+        biblioteka.wypiszDostepneKsiazki();
+        System.out.println("Liczba dostępnych: " + biblioteka.policzDostepneKsiazki());
+
+        // Zwrot 
+        System.out.println("\n=== Zwrot książki ===");
+        biblioteka.zwrocKsiazke("Potop", c1);
+
+        System.out.println("\n=== Dostępne książki po zwrocie ===");
+        biblioteka.wypiszDostepneKsiazki();
+        System.out.println("Liczba dostępnych: " + biblioteka.policzDostepneKsiazki());
+
+        // Stan czytelników 
+        System.out.println("\n=== Stan czytelników ===");
+        c1.wypiszDane();
+        c2.wypiszDane();
+    }
+
